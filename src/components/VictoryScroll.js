5204dcf1ef16024231bdb01e66bdc8ee64a5b283
Douglas Uretsky <douglasuretsky@gmail.com>
2019-08-06 13:37:43 -0400

    create VictoryScroll component to display if player wins game

---
+++ src/components/VictoryScroll.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+
+class VictoryScroll extends Component {
+  
+  render() {
+      return (
+      <div id="victory-scroll">
+        </div>
+      );
+    }
+  }
+
+export default VictoryScroll
